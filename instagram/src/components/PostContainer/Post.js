4ecb918e3183bd0f2fd5bd30b009058e31bd0fb4
Mario Amaya <thefozcat@gmail.com>
2019-03-26 14:56:48 -0700

    added 'addLikes' function to post

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Post = props => {
-    return (
-        <div className="post">
-            <div className="user-tag">
-                <img src={props.thumbnailUrl} alt="profile"/>
-                <h2>{props.username}</h2>
+class Post extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            likes: props.likes
+        }
+    }
+
+    // Add Likes Function
+
+    addLikes = event => {
+        event.preventDefault();
+        this.setState({
+            likes: this.state.likes + 1
+        })
+    }
+
+    // Render
+
+    render() {
+        return (
+            <div className="post">
+                <div className="user-tag">
+                    <img src={this.props.thumbnailUrl} alt="profile"/>
+                    <h2>{this.props.username}</h2>
+                </div>
+                <img src={this.props.imageUrl} alt="post"/>
+                <div className="post-icons">
+                    <i className="far fa-heart" onClick={this.addLikes}></i><i className="far fa-comment"></i>
+                </div>
+                <p className="likes"><strong>{this.state.likes} likes</strong></p>
             </div>
-            <img src={props.imageUrl} alt="post"/>
-            <div className="post-icons">
-                <i className="far fa-heart"></i><i className="far fa-comment"></i>
-            </div>
-            <p className="likes"><strong>{props.likes} likes</strong></p>
-        </div>
-    );
+        );
+    }
 }
 
 // Prop Types
